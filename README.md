c6583ff88fa2c923dff35e6c5b2abb8c71464673
Pallav Bakshi <pallavbakshi@gmail.com>
2023-09-09 00:56:33 +0800

    feat: Implement file renaming script with revert and ignore options
    This commit introduces a Python script that renames files in a specified folder or a single file. The renaming follows certain conventions such as replacing spaces with hyphens, converting to lowercase, and removing special characters. The script also provides a revert option to undo the renaming, and an ignore option to exclude certain files from being renamed. Hidden files (dotfiles) are automatically ignored.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # file-renamer
 Rename files in a folder
+
+## What is this?
+This is a simple script that renames files in a folder. It can be used to rename files in a folder to follow a particular set of conventions. 
+
+## How to use?
+- To rename all files in a folder
+    - `python file_renamer.py --folder <folder_path>`
+- To rename a single file
+    - `python file_renamer.py --file <file_path>`
+
+## What are the conventions?
+- Replace all spaces with hyphen (-).
+- Lowercase all letters.
+- Remove all special characters except hyphen (-) and underscore (_).
+- Remove multiples (double/tripple) hypens (--).
+
+## How to ignore files?
+- By default, all dotfiles are ignored.
+- You can ignore files by using the `--ignore` flag.
+    - `python file_renamer.py --folder <folder_path> --ignore <file_name>` 
+
+## How to revert the changes?
+Sometimes you don't like the renaming and want to revert the changes. You can do that by using the `--revert` flag. 
+
+Under the hood, the script creates a dotfile called `.file_renamer.csv` in the folder. This file contains the original file names and the renamed file names. When you use the `--revert` flag, the script reads this file and reverts the changes.
---
+++ file_renamer.py
@@ -0,0 +1,96 @@
+import os
+import re
+import csv
+import click
+
+
+@click.command()
+@click.option("--folder", help="The folder containing the files to rename.")
+@click.option("--file", help="A single file to rename.")
+@click.option(
+    "--revert",
+    is_flag=True,
+    help="Revert the file names to their original state.",
+)
+@click.option(
+    "--ignore",
+    type=click.Path(exists=True),
+    multiple=True,
+    help="File path to ignore when renaming. You can give multiple file paths using --ignore <file_path> multiple times. Give complete file path.",
+)
+def rename_files(folder, file, revert, ignore):
+    if revert:
+        if folder:
+            with open(os.path.join(folder, ".file_renamer.csv"), "r") as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    os.rename(
+                        os.path.join(folder, row[1]),
+                        os.path.join(folder, row[0]),
+                    )
+        elif file:
+            filename = os.path.basename(file)
+            with open(
+                os.path.join(os.path.dirname(file), ".file_renamer.csv"), "r"
+            ) as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if row[1] == filename:
+                        os.rename(
+                            file, os.path.join(os.path.dirname(file), row[0])
+                        )
+                        break
+        else:
+            click.echo("Please provide either a folder or a file to revert.")
+    elif folder:
+        with open(os.path.join(folder, ".file_renamer.csv"), "w") as f:
+            writer = csv.writer(f)
+            for filename in os.listdir(folder):
+                if is_file_to_be_ignored(filename, ignore):
+                    continue
+                new_filename = apply_rename_conventions(filename)
+                writer.writerow([filename, new_filename])
+                os.rename(
+                    os.path.join(folder, filename),
+                    os.path.join(folder, new_filename),
+                )
+    elif file:
+        if is_file_to_be_ignored(file, ignore):
+            click.echo(
+                "Please provide a valid file to rename. This file cannot be renamed because it's a hidden file (dotfile) or is in the ignore list."
+            )
+            return
+
+        filename = os.path.basename(file)
+        new_filename = apply_rename_conventions(filename)
+        with open(
+            os.path.join(os.path.dirname(file), ".file_renamer.csv"), "w"
+        ) as f:
+            writer = csv.writer(f)
+            writer.writerow([filename, new_filename])
+        os.rename(file, os.path.join(os.path.dirname(file), new_filename))
+    else:
+        click.echo("Please provide either a folder or a file to rename.")
+
+
+def is_file_to_be_ignored(filename: str, ignore: list) -> bool:
+    return filename.startswith(".") or any(
+        ignored_file == filename for ignored_file in ignore
+    )
+
+
+def apply_rename_conventions(filename: str) -> str:
+    """Apply renaming conventions to a filename."""
+    filename = filename.replace(" ", "-").lower()
+
+    # remove all special characters
+    filename = re.sub(r"[^a-zA-Z0-9_.-]", "", filename)
+
+    # remove multiple consecutive hyphens
+    filename = re.sub(r"-+", "-", filename)
+
+    return filename
+
+
+if __name__ == "__main__":
+    rename_files()
